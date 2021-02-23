31495804d8220435d90e35e603193cd1efd01be4
nkeum <rmaskdus0208@gmail.com>
2021-02-23 14:49:21 +0900

    models.py 수정
    updated_at

--- todoapp/models.py
+++ todoapp/models.py
@@ -9,11 +9,11 @@ class Post(models.Model):
     title = models.CharField(help_text="Post title", max_length=100, blank=False, null=False, default="Enter title")
     contents = models.TextField(help_text="post contents", blank=False, null=False)
     created_at=models.DateTimeField(auto_now_add=True, help_text="post created date")
-    updated_at=models.DateTimeField(auto_now_add=True, help_text="post updated date")
+    updated_at=models.DateTimeField(auto_now=True, help_text="post updated date")
 
 class Comment(models.Model):
     id = models.BigAutoField(help_text="Comment ID", primary_key=True)
     post_id = models.ForeignKey("Post", related_name="post", on_delete=models.CASCADE, db_column="post_id")
     contents = models.TextField(help_text="Comment contents", blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True, help_text="post created date")
-    updated_at = models.DateTimeField(auto_now_add=True, help_text="post updated date")
+    updated_at=models.DateTimeField(auto_now=True, help_text="post updated date")
